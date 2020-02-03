e56444ac156ded65ea2a8862ec671bffed9a8616
command7 <vijay_saravanan@outlook.com>
2020-02-03 18:58:12 -0500

    Feat: Add getter and setter methods for private variables titleId and fileName in TitleRating class

--- src/com/datageneration/TitleRating.java
+++ src/com/datageneration/TitleRating.java
@@ -1,11 +1,35 @@
 package com.datageneration;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.HashMap;
+
 public class TitleRating {
     private String titleId;
+    private String fileName;
 
-    public TitleRating(String _titleId) {
+
+    public String getTitleId() {
+        return this.titleId;
+    }
+
+    public String getFileName() {
+        return this.fileName;
+    }
+
+    public void setTitleId(String _titleId) {
         this.titleId = _titleId;
     }
+
+    public void setFileName(String _fileName) {
+        this.fileName = _fileName;
+    }
+
+    public TitleRating(String _titleId) {
+        this.setTitleId(_titleId);
+        this.setFileName("title_ratings.csv");
+    }
+
 
 
 }
