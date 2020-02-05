e81382b1ce7efba92be64f1ecef4e7cf5d575d72
command7 <vijay_saravanan@outlook.com>
2020-02-05 18:09:14 -0500

    Feat: TitlePrincipals reads and filters data from title_principals.csv

--- src/com/datageneration/TitlePrincipals.java
+++ src/com/datageneration/TitlePrincipals.java
@@ -1,4 +1,17 @@
 package com.datageneration;
 
-public class TitlePrincipals {
+public class TitlePrincipals extends DataUnit{
+
+    public TitlePrincipals(String _titleId) {
+        this.setTitleId(_titleId);
+        this.setFileName("title_principals.csv");
+        this.initializeFileData();
+        this.openCsvFile();
+        this.filter();
+    }
+
+    public static void main(String[] args) {
+        TitlePrincipals test = new TitlePrincipals("tt0000001");
+        System.out.println(test);
+    }
 }
