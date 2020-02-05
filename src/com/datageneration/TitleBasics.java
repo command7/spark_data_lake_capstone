355b14f1ff8909ecf229d2c4c761836eb29ba2aa
command7 <vijay_saravanan@outlook.com>
2020-02-05 18:06:54 -0500

    Feat: TitleBasics reads and filters data from title_basics.csv

--- src/com/datageneration/TitleBasics.java
+++ src/com/datageneration/TitleBasics.java
@@ -1,4 +1,17 @@
 package com.datageneration;
 
-public class TitleBasics {
+public class TitleBasics extends DataUnit{
+
+    public TitleBasics(String _titleId) {
+        this.setTitleId(_titleId);
+        this.setFileName("title_basics.csv");
+        this.initializeFileData();
+        this.openCsvFile();
+        this.filter();
+    }
+
+    public static void main(String[] args) {
+        TitleBasics test = new TitleBasics("tt0000001");
+        System.out.println(test);
+    }
 }
