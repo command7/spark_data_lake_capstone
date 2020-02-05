861c09a68b1dc1465814c1de15ecfa3411aa6c34
command7 <vijay_saravanan@outlook.com>
2020-02-05 18:24:43 -0500

    Feat: Override convertToJson() in TitlePrincipals class to accommodate diverse data

--- src/com/datageneration/TitlePrincipals.java
+++ src/com/datageneration/TitlePrincipals.java
@@ -1,5 +1,9 @@
 package com.datageneration;
 
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
 public class TitlePrincipals extends DataUnit{
 
     public TitlePrincipals(String _titleId) {
@@ -10,8 +14,20 @@ public class TitlePrincipals extends DataUnit{
         this.filter();
     }
 
+    @Override
+    public JSONObject convertToJson(ArrayList<String> rowData) {
+        JSONObject dataInJson = new JSONObject();
+        dataInJson.put("tconst", rowData.get(0));
+        dataInJson.put("ordering", Integer.valueOf(rowData.get(1)));
+        dataInJson.put("nconst", rowData.get(2));
+        dataInJson.put("category", rowData.get(3));
+        dataInJson.put("job", rowData.get(4));
+        dataInJson.put("characters", rowData.get(5));
+        return dataInJson;
+    }
+
     public static void main(String[] args) {
         TitlePrincipals test = new TitlePrincipals("tt0000001");
-        System.out.println(test);
+        System.out.println(test.getDataAsJson());
     }
 }
