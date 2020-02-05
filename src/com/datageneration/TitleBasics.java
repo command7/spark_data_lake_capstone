c2b71712a52903daa690c03ce766be53db9aaf0f
command7 <vijay_saravanan@outlook.com>
2020-02-05 18:33:06 -0500

    Feat: Override convertToJson() in TitleBasics class to accommodate change in data fields

--- src/com/datageneration/TitleBasics.java
+++ src/com/datageneration/TitleBasics.java
@@ -1,5 +1,9 @@
 package com.datageneration;
 
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
 public class TitleBasics extends DataUnit{
 
     public TitleBasics(String _titleId) {
@@ -10,8 +14,24 @@ public class TitleBasics extends DataUnit{
         this.filter();
     }
 
+    @Override
+    public JSONObject convertToJson(ArrayList<String> rowData) {
+        JSONObject dataInJson = new JSONObject();
+
+        dataInJson.put("tconst", rowData.get(0));
+        dataInJson.put("titleType", rowData.get(1));
+        dataInJson.put("primaryTitle", rowData.get(2));
+        dataInJson.put("originalTitle", rowData.get(3));
+        dataInJson.put("isAdult", Integer.valueOf(rowData.get(4)));
+        dataInJson.put("startYear", Integer.valueOf(rowData.get(5)));
+        dataInJson.put("endYear", rowData.get(6));
+        dataInJson.put("runTimeMinutes", Integer.valueOf(rowData.get(7)));
+
+        return dataInJson;
+    }
+
     public static void main(String[] args) {
         TitleBasics test = new TitleBasics("tt0000001");
-        System.out.println(test);
+        System.out.println(test.getDataAsJson());
     }
 }
