e5c5b77f52c208c6c8429199ff749135ac5bbb37
command7 <vijay_saravanan@outlook.com>
2020-02-05 19:22:19 -0500

    Feat: Add getTitleIdAtIndex() to DataGenerator that reads the csv file and returns the titleId present at specified index

--- src/com/datageneration/DataGenerator.java
+++ src/com/datageneration/DataGenerator.java
@@ -2,6 +2,8 @@ package com.datageneration;
 
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.util.ArrayList;
 
 public class DataGenerator {
@@ -9,6 +11,27 @@ public class DataGenerator {
 
     public int getRandomNumber(int endingNumber) {
        return (int)(Math.random()*((endingNumber)+1));
+    }
+
+    public String getTitleIdAtIndex(String fileName, int itemIndex) {
+        int currentLineNumber = 1;
+        String outputTitleId = "";
+        String dataLine;
+        try {
+            BufferedReader csvReader = new BufferedReader(new FileReader(fileName));
+            while ((dataLine = csvReader.readLine()) != null) {
+                if (currentLineNumber == itemIndex) {
+                    outputTitleId = dataLine;
+                    break;
+                }
+                else {
+                    currentLineNumber += 1;
+                }
+            }
+            csvReader.close();
+        }
+        catch (Exception ex) {};
+        return outputTitleId;
     }
 
     public DataGenerator() {
