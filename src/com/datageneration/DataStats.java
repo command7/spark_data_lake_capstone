e8d4f0193e811ff17488a9305589b9c92dd3321f
command7 <vijay_saravanan@outlook.com>
2020-02-09 17:36:57 -0500

    Feat: Add connect() to DataStats class that connects to the database and returns the connection object

--- src/com/datageneration/DataStats.java
+++ src/com/datageneration/DataStats.java
@@ -1,11 +1,13 @@
 package com.datageneration;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 
 public class DataStats {
     private static String connectionString = "jdbc:mysql://localhost:3306/imdb_capstone?useSSL=false";
     private static String userName = "root";
     private static String password = "student";
+    private static Connection connection;
 
     private String getConnectionString() {
         return connectionString;
@@ -17,5 +19,15 @@ public class DataStats {
 
     private String getPassword() {
         return password;
+    }
+
+    public Connection connect() {
+        try {
+            connection = DriverManager.getConnection(connectionString, getUserName(), getPassword());
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        return connection;
     }
 }
