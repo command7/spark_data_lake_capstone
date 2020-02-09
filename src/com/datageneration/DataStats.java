718d1e4de85a13afca310d5f6d936e6780f2af61
command7 <vijay_saravanan@outlook.com>
2020-02-09 17:34:33 -0500

    Feat: Declare connection string, username and password as instance variables and provide getter methods

--- src/com/datageneration/DataStats.java
+++ src/com/datageneration/DataStats.java
@@ -1,5 +1,21 @@
 package com.datageneration;
 
-public class DataStats {
+import java.sql.Connection;
 
+public class DataStats {
+    private static String connectionString = "jdbc:mysql://localhost:3306/imdb_capstone?useSSL=false";
+    private static String userName = "root";
+    private static String password = "student";
+
+    private String getConnectionString() {
+        return connectionString;
+    }
+
+    private String getUserName() {
+        return userName;
+    }
+
+    private String getPassword() {
+        return password;
+    }
 }
