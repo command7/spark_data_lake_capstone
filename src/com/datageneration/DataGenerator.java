92f7120c60883dafc69cbd0f0df1ee893e1b95c8
command7 <vijay_saravanan@outlook.com>
2020-02-09 18:15:03 -0500

    Feat: Add getTitleToProcess() to DataGenerator that returns a random titleId

--- src/com/datageneration/DataGenerator.java
+++ src/com/datageneration/DataGenerator.java
@@ -12,26 +12,31 @@ public class DataGenerator {
        int endingNumber = DataStats.getRemainingRecords();
         return (int)(Math.random()*((endingNumber)+1));
     }
+//
+//    public String getTitleIdAtIndex(String fileName, int itemIndex) {
+//        int currentLineNumber = 1;
+//        String outputTitleId = "";
+//        String dataLine;
+//        try {
+//            BufferedReader csvReader = new BufferedReader(new FileReader(fileName));
+//            while ((dataLine = csvReader.readLine()) != null) {
+//                if (currentLineNumber == itemIndex) {
+//                    outputTitleId = dataLine;
+//                    break;
+//                }
+//                else {
+//                    currentLineNumber += 1;
+//                }
+//            }
+//            csvReader.close();
+//        }
+//        catch (Exception ex) {};
+//        return outputTitleId;
+//    }
 
-    public String getTitleIdAtIndex(String fileName, int itemIndex) {
-        int currentLineNumber = 1;
-        String outputTitleId = "";
-        String dataLine;
-        try {
-            BufferedReader csvReader = new BufferedReader(new FileReader(fileName));
-            while ((dataLine = csvReader.readLine()) != null) {
-                if (currentLineNumber == itemIndex) {
-                    outputTitleId = dataLine;
-                    break;
-                }
-                else {
-                    currentLineNumber += 1;
-                }
-            }
-            csvReader.close();
-        }
-        catch (Exception ex) {};
-        return outputTitleId;
+    public String getTitleToProcess() {
+        int idIndex = getRandomNumber();
+        return DataStats.getIdAtIndex(idIndex);
     }
 
 
