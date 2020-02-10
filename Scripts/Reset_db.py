1c4ae0c34e364c56a8ba32a37d39ba48589b128d
command7 <vijay_saravanan@outlook.com>
2020-02-10 00:39:06 -0500

    Feat: Reset_db.py now resets the name_stats table in imdb_capstone database

--- Scripts/Reset_db.py
+++ Scripts/Reset_db.py
@@ -1,27 +1,27 @@
 import pandas as pd
 import pymysql
-#
-# df = pd.read_csv("../Data/title_basics.csv",
-#                   low_memory=False)
-# unique_tconsts = list(df.tconst.unique())
-# del df
-#
+
+df = pd.read_csv("../Data/title_basics.csv",
+                  low_memory=False)
+unique_tconsts = list(df.tconst.unique())
+del df
+
 db_connection = pymysql.connect(host="localhost",
                                 user="root",
                                 password="student",
                                 db="imdb_capstone",
                                 autocommit=True)
 db_cursor = db_connection.cursor()
-#
-# drop_imdb_stats_sql = "DROP TABLE IF EXISTS imdb_stats"
-# create_imdb_stats_sql = "CREATE TABLE IF NOT EXISTS imdb_stats (tconst varchar(20) PRIMARY KEY)"
-# insert_imdb_stats_sql = "INSERT INTO `imdb_stats` (`tconst`) VALUES (%s)"
-#
-# db_cursor.execute(drop_imdb_stats_sql)
-# db_cursor.execute(create_imdb_stats_sql)
-#
-# for each_tconst in unique_tconsts:
-#     db_cursor.execute(insert_data_sql, (each_tconst))
+
+drop_imdb_stats_sql = "DROP TABLE IF EXISTS imdb_stats"
+create_imdb_stats_sql = "CREATE TABLE IF NOT EXISTS imdb_stats (tconst varchar(20) PRIMARY KEY)"
+insert_imdb_stats_sql = "INSERT INTO `imdb_stats` (`tconst`) VALUES (%s)"
+
+db_cursor.execute(drop_imdb_stats_sql)
+db_cursor.execute(create_imdb_stats_sql)
+
+for each_tconst in unique_tconsts:
+    db_cursor.execute(insert_imdb_stats_sql, (each_tconst))
 
 drop_name_stats_sql = "DROP TABLE IF EXISTS name_stats"
 create_name_stats_sql =  """
