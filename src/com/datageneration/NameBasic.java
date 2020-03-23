26e947e7ff30c3f152ed2b1c82710e3dad04da88
command7 <vijay_saravanan@outlook.com>
2020-03-23 17:32:14 -0400

    Misc: Remove unnecessary convertToJson() in NameBasic

--- src/com/datageneration/NameBasic.java
+++ src/com/datageneration/NameBasic.java
@@ -15,20 +15,6 @@ public class NameBasic extends DataUnit{
         this.openCsvFile();
     }
 
-    @Override
-    public JSONObject convertToJson(ArrayList<String> rowData) {
-        JSONObject dataInJson = new JSONObject();
-
-        this.insertIntoJsonObject(dataInJson, "nconst", rowData.get(0), "string");
-        this.insertIntoJsonObject(dataInJson, "primaryName", rowData.get(1), "string");
-        this.insertIntoJsonObject(dataInJson, "birthYear", rowData.get(2), "integer");
-        this.insertIntoJsonObject(dataInJson, "deathYear", rowData.get(3), "integer");
-        this.insertIntoJsonObject(dataInJson, "primaryProfession", rowData.get(4), "string");
-        this.insertIntoJsonObject(dataInJson, "knownForTitles", rowData.get(5), "string");
-
-        return dataInJson;
-    }
-
     public static void main(String[] args) {
         NameBasic test = new NameBasic("nm0000001");
         System.out.println(test.getDataAsJson());
