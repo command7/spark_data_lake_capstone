563df922775fcc920622ee210400bb1e8a801d43
command7 <vijay_saravanan@outlook.com>
2020-03-29 22:59:54 -0400

    Refactor: Use csvreader instead of pandas to optimize speed of execution

--- Scripts/Reset_db.py
+++ Scripts/Reset_db.py
@@ -1,27 +1,5 @@
-import pandas as pd
 import pymysql
-
-df = pd.read_csv("../Data/title_basics.csv",
-                  low_memory=False)
-unique_tconsts = list(df.tconst.unique())
-del df
-
-db_connection = pymysql.connect(host="localhost",
-                                user="root",
-                                password="student",
-                                db="imdb_capstone",
-                                autocommit=True)
-db_cursor = db_connection.cursor()
-
-drop_imdb_stats_sql = "DROP TABLE IF EXISTS imdb_stats"
-create_imdb_stats_sql = "CREATE TABLE IF NOT EXISTS imdb_stats (tconst varchar(20) PRIMARY KEY)"
-insert_imdb_stats_sql = "INSERT INTO `imdb_stats` (`tconst`) VALUES (%s)"
-
-db_cursor.execute(drop_imdb_stats_sql)
-db_cursor.execute(create_imdb_stats_sql)
-
-for each_tconst in unique_tconsts:
-    db_cursor.execute(insert_imdb_stats_sql, (each_tconst))
+import csv
 
 drop_name_stats_sql = "DROP TABLE IF EXISTS name_stats"
 create_name_stats_sql =  """
@@ -31,22 +9,54 @@ CREATE TABLE IF NOT EXISTS name_stats (
     PRIMARY KEY (tconst, nconst)
 )
 """
+drop_imdb_stats_sql = "DROP TABLE IF EXISTS imdb_stats"
+create_imdb_stats_sql = "CREATE TABLE IF NOT EXISTS imdb_stats (tconst varchar(20) PRIMARY KEY)"
+insert_imdb_stats_sql = "INSERT INTO `imdb_stats` (`tconst`) VALUES (%s)"
 insert_name_stats_sql = "INSERT INTO `name_stats` (`tconst`, `nconst`) VALUES (%s, %s)"
 
-df = pd.read_csv("../Data/title_principals.csv",
-                  low_memory=False)
-nconst_tconst_couples = df[["tconst", "nconst"]].to_numpy()
 
-combo_uniques = set()
-for each_couple in nconst_tconst_couples:
-    combo_uniques.add(tuple(each_couple))
+unique_tconsts = set()
+with open("../Data/title_basics_sampled.csv") as title_basics_file:
+    title_basics_reader = csv.reader(title_basics_file, delimiter=",")
+    for each_id_row in title_basics_reader:
+        unique_tconsts.add(each_id_row[0])
 
-del df
+print("Unique tconsts gathered")
+
+
+db_connection = pymysql.connect(host="localhost",
+                                user="root",
+                                password="rootstudent",
+                                db="imdb_capstone",
+                                autocommit=True)
+db_cursor = db_connection.cursor()
+
+
+
+db_cursor.execute(drop_imdb_stats_sql)
+db_cursor.execute(create_imdb_stats_sql)
+
+for each_tconst in unique_tconsts:
+    db_cursor.execute(insert_imdb_stats_sql, (each_tconst))
+
+print("Filled imdb_stats table")
+
+
+
+print("\n\n Starting to fill name_stats")
+
+unique_principal_rows = set()
+with open("../Data/title_principals_sampled.csv") as title_principals_file:
+    title_principals_reader = csv.reader(title_principals_file, delimiter=",")
+    for each_id_row in title_principals_reader:
+        unique_principal_rows.add((each_id_row[0], each_id_row[2]))
+
+print("Found unique tconst nconst rows")
 
 db_cursor.execute(drop_name_stats_sql)
 db_cursor.execute(create_name_stats_sql)
 
-for tconst, nconst in combo_uniques:
+for tconst, nconst in unique_principal_rows:
     db_cursor.execute(insert_name_stats_sql, (tconst, nconst))
 
-
+print("Filled name_stats table")
