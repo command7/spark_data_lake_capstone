79c8e4de226543b9757ce319af19bc72e19b42cd
command7 <vijay_saravanan@outlook.com>
2020-03-30 14:53:52 -0400

    Feat: NameBasic.java logs name ids that are not found to RealTimeData/Errors/NameBasicsErrors.txt

--- src/com/datageneration/NameBasic.java
+++ src/com/datageneration/NameBasic.java
@@ -2,13 +2,32 @@ package com.datageneration;
 
 import org.json.JSONObject;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.LinkedList;
 
 public class NameBasic extends DataUnit{
 
+    private BufferedWriter errorWriter;
+
+    public BufferedWriter getErrorWriter() {
+        return errorWriter;
+    }
+
+    private void initializeErrorWriter() {
+        try {
+            this.errorWriter = new BufferedWriter(
+                    new FileWriter(new File("RealTimeData/Errors/NameBasicsErrors.txt")));
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
     public NameBasic() {
         super("name_basics.csv");
+        initializeErrorWriter();
     }
 
     public LinkedList<JSONObject> getAllRecordsForTitleAsJson(String uniqueIdForRow) {
@@ -16,6 +35,17 @@ public class NameBasic extends DataUnit{
         ArrayList<String> nameIds = DataStats.getNameConstsForTitle(uniqueIdForRow);
         for (String nameId: nameIds) {
             JSONObject individualNameData = this.getSingleRecordAsJson(nameId);
+            if (individualNameData == null) {
+                try {
+                    this.getErrorWriter().write(nameId);
+                    this.getErrorWriter().newLine();
+                    this.getErrorWriter().flush();
+                    System.out.println(nameId);
+                }
+                catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
             dataAsJsonObjects.addLast(individualNameData);
         }
 
